0e989c1d75e317b0672caf3c9af93f25104e6a47
Jesus Santos Barrenechea <u201312919@upc.edu.pe>
2017-05-10 16:24:46 +0000

    H

--- index.php
+++ index.php
@@ -1,21 +1,41 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
-</html>
+ <html>
+ <body>
+ <div name="top" align="center">
+<img class = 'newappIcon' src='images/git.PNG' style='width: 66%;''>
+ </div>
+ <div name="middle" align="center">
+
+	  	<?php
+		$servername = "us-cdbr-iron-east-03.cleardb.net";
+		$username = "b74ba3320e82ec";
+		$password = "2d194843";
+		$dbname = "ad_bb6ddb2f34daf52";
+		
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		if ($conn->connect_error) {
+		  die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * from servicios";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) { ?>
+		<table>
+		  <?php while($row = $result->fetch_assoc()) { ?>
+		  	<tr style="width: 50%; float: left; height: 114px;">
+		      <td><img src="<?php echo $row['imagen_servicio'] ?>" /></td>
+		      <td><b><?php echo $row['nombre_servicio']?></b> <br /> <?php echo $row['descripcion_servicio']?></td>
+		    </tr>
+		  <?php } ?>
+		</table>
+		<?php }  
+		$conn->close();
+		?> 
+	
+	 </div>
+ <div name="bottom" align="center">
+		<img class = 'newappIcon' src='images/sas.PNG'>
+ </div>
+  
+ </body>
+ </html>
